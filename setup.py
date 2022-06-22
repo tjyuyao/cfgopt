1570bf822fb78c6e448cc91f8893fcc4c726bbe4
Yuyao Huang <huangyuyao@outlook.com>
2022-06-22 08:06:05 +0800

    v0.4.1 pypi friendly README

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+# read the contents of README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='cfgopt',
-      version='0.4.0',
+      version='0.4.1',
       description='You only configure your deep learning experiment once with cfgopt.',
       url='http://github.com/tjyuyao/cfgopt',
       author='Yuyao Huang',
@@ -11,4 +16,6 @@ setup(name='cfgopt',
       entry_points={
           'console_scripts': ['cfgoptrun=cfgopt.main:main'],
       },
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       zip_safe=False)
